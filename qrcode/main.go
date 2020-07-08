1c0ee1803fbf9a381c73535f6bb859c4948ad13e
Rui Pereira <rui@anchorlabs.com>
2020-07-08 18:46:44 +0000

    Overlay Anchorage logo

--- qrcode/main.go
+++ qrcode/main.go
@@ -4,15 +4,19 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
 	"os"
-	"strings"
 
 	qrcode "github.com/skip2/go-qrcode"
 )
 
 func main() {
+	// content := flag.String("c", "", "content to encode")
 	outFile := flag.String("o", "", "out PNG file prefix, empty for stdout")
 	size := flag.Int("s", 256, "image size (pixel)")
 	textArt := flag.Bool("t", false, "print as text-art on stdout")
@@ -47,39 +51,51 @@ Usage:
 		checkError(fmt.Errorf("Error: no content given"))
 	}
 
-	content := strings.Join(flag.Args(), " ")
+	args := flag.Args()
+	content := args[0]
+	logoPath := args[1]
 
 	var err error
-	var q *qrcode.QRCode
-	q, err = qrcode.New(content, qrcode.Highest)
+	var code *qrcode.QRCode
+	code, err = qrcode.New(content, qrcode.Highest)
 	checkError(err)
 
 	if *disableBorder {
-		q.DisableBorder = true
+		code.DisableBorder = true
 	}
 
 	if *textArt {
-		art := q.ToString(*negative)
+		art := code.ToString(*negative)
 		fmt.Println(art)
 		return
 	}
 
 	if *negative {
-		q.ForegroundColor, q.BackgroundColor = q.BackgroundColor, q.ForegroundColor
+		code.ForegroundColor, code.BackgroundColor = code.BackgroundColor, code.ForegroundColor
 	}
 
-	var png []byte
-	png, err = q.PNG(*size)
+	var buf bytes.Buffer
+	img := code.Image(*size)
+
+	file, err := os.Open(logoPath)
+	checkError(err)
+	defer file.Close()
+
+	logo, _, err := image.Decode(file)
+	checkError(err)
+	overlayLogo(img, logo, code)
+
+	err = png.Encode(&buf, img)
 	checkError(err)
 
 	if *outFile == "" {
-		os.Stdout.Write(png)
+		os.Stdout.Write(buf.Bytes())
 	} else {
 		var fh *os.File
 		fh, err = os.Create(*outFile + ".png")
 		checkError(err)
 		defer fh.Close()
-		fh.Write(png)
+		fh.Write(buf.Bytes())
 	}
 }
 
@@ -87,5 +103,26 @@ func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
+	}
+}
+
+var GreyThreshold = 30
+var AlphaThreshold = 2000
+
+// overlayLogo blends logo to the center of the QR code.
+func overlayLogo(dst, src image.Image, qrcode *qrcode.QRCode) {
+	grey := uint32(^uint16(0)) * uint32(GreyThreshold) / 100
+	alphaOffset := uint32(AlphaThreshold)
+	offset := dst.Bounds().Max.X/2 - src.Bounds().Max.X/2
+	for x := 0; x < src.Bounds().Max.X; x++ {
+		for y := 0; y < src.Bounds().Max.Y; y++ {
+			if r, g, b, alpha := src.At(x, y).RGBA(); alpha > alphaOffset {
+				col := qrcode.ForegroundColor
+				if r > grey && g > grey && b > grey {
+					col = color.White
+				}
+				dst.(*image.Paletted).Set(x+offset, y+offset, col)
+			}
+		}
 	}
 }
